bc792598d554a60b42690706e58cde6fb9454f5f
Mungo Dewar <mpjdewar@gmail.com>
2017-06-30 15:08:29 +0100

    add body parser

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use(express.static('client/build') );
 
