7327d91a2bef0329e8306ad3bad5d6e6b8cd5c1b
Mungo Dewar <mpjdewar@gmail.com>
2017-06-30 14:20:58 +0100

    add db connection

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
 
 app.get('/', function (req, res) {
-  res.json({name: "I AM ALAN, WELCOME TO THE SHIT SHOW"})
+  MongoClient.connect('mongodb://localhost:27017/gigs', function(err, db){
+    if (err) return;
+    db.collection('listings').find().toArray(function(err, docs){
+      res.json(docs);
+    })
+  })
 });
 
 app.listen(3000, function () {
