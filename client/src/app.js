178bd7c9039b9df93901c78f4fba9352fb39b8a3
Mungo Dewar <mpjdewar@gmail.com>
2017-06-30 14:50:47 +0100

    add front end connection to database

--- client/src/app.js
+++ client/src/app.js
@@ -1,1 +1,12 @@
-console.log("hello");
+//console.log("hello");
+
+var app = function(){
+  var request = new XMLHttpRequest();
+  request.open('GET', 'http://localhost:3000/api/listings');
+  request.onload = function(){
+    console.log(request.responseText);
+  }
+  request.send();
+}
+
+app();
