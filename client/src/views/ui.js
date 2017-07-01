8f91cbbfb1df38b34d654e912afc03c8ea724245
Louise Camlin <lacamlin@gmail.com>
2017-07-01 11:44:42 +0100

    create ui object and prototype and add some make html element functions

--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -1,11 +1,29 @@
+var request = require('../request.js');
 
+var UI = function(){
+  this.request();
+}
 
+UI.Prototype ={
 
+  makeList: function(){
+    var body = getElementById('#main-body')
+    var div = createElement('div');
+    var ul = createElement('ul');
+    var li = createElement('li');
+    ul.appendChild(li);
+    div.appendChild(ul);
+    body.appendChild(div);
 
+  },
 
+  makeSearchInput: function() {
+    var body = getElementById('#main-body')
+    var search = createElement('input');
+    body.appendChild(search);
 
-
-
+  }
+}
 
 
 module.exports = UI;
