4938fb9dbfbc71ea4084105029fc0817199dee33
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-07-03 21:13:24 +0100

    partial fix

--- client/src/views/displayInfo.js
+++ client/src/views/displayInfo.js
@@ -6,16 +6,16 @@ var DisplayInfo = function(){
 
 DisplayInfo.prototype = {
   makeMapInfo: function(event){
-    console.log(event);
-    var body = document.getElementsByTagName('body')[0]
+    // var body = document.getElementsByTagName('body')[0]
     var popUp = document.createElement('div');
     this.makeMap(popUp, {lat: 55.953251, lng:-3.188267})
     popUp.id = 'pop-up';
     event.target.appendChild(popUp);
+
   },
   makeMap: function(el, center){
     var mainMap = new MapWrapper(el, center, 10);
-    mainMap.addMarker(center)
+    mainMap.addMarker(center);
   }
 }
 
--- client/src/views/list.js
+++ client/src/views/list.js
@@ -1,8 +1,9 @@
 var Gigs = require('../models/gigs.js');
 var MapWrapper = require('../mapWrapper.js');
 var DisplayInfo = require('./displayInfo.js');
-var List = function () {
 
+var List = function () {
+  this.currentMap;
 };
 
 List.prototype = {
@@ -17,12 +18,23 @@ List.prototype = {
         var gigListLi = document.createElement('li');
         gigListLi.innerText = gig.name;
         gigListLi.addEventListener('click', function(event){
+          console.log(this.currentMap !== event.target);
+          if (this.currentMap !== event.target){
+            var existingPopUp = document.querySelector('#pop-up');
+            if (existingPopUp){
+              var li = existingPopUp.parentNode;
+              li.removeChild(existingPopUp);
+            } 
+          }
+          this.currentMap = event.target;
+          console.log("after currentMap", this.currentMap);
+          console.log("after event", event.target)
           var displayInfo = new DisplayInfo();
           displayInfo.makeMapInfo(event);
-        })
+        }.bind(this))
         ul.appendChild(gigListLi);
       }
-    });
+    }.bind(this));
     listDiv.appendChild(ul);
     body.appendChild(listDiv);
   }
