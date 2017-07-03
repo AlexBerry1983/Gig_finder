ebb9213bdec8bbf3c85c2cd96119851b68e7915d
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-07-03 13:22:35 +0100

    refactor HTML

--- client/src/views/displayInfo.js
+++ client/src/views/displayInfo.js
@@ -6,12 +6,12 @@ var DisplayInfo = function(){
 
 DisplayInfo.prototype = {
   makeMapInfo: function(){
-    var mainDiv = document.getElementById('main-div');
+    var body = document.getElementsByTagName('body')[0]
     var popUp = document.createElement('div');
     console.log(this);
     this.makeMap(popUp, {lat: 55.953251, lng:-3.188267})
     popUp.id = 'pop-up';
-    mainDiv.appendChild(popUp);
+    body.appendChild(popUp);
   },
   makeMap: function(el, center){
     var mainMap = new MapWrapper(el, center, 10);
