f07237a1613035434d3335877b950002df9a5806
Mungo Dewar <mpjdewar@gmail.com>
2017-07-03 16:55:55 +0100

    dropdown rendering

--- client/src/views/dropdown.js
+++ client/src/views/dropdown.js
@@ -1,10 +1,22 @@
-var Dropdown = function () {
-
+var Dropdown = function (inputHook) {
+  this.ul = document.createElement('li');
+  this.ul.textContent = 'test';
+  inputHook.insertAdjacentElement("beforebegin", this.ul);
+  console.log(inputHook.parentNode);
 }
 
 Dropdown.prototype = {
   render: function (objs) {
-    console.log(objs);
+    for (gig of objs._embedded.events) {
+      console.log(this.ul);
+      var gigLi = this.createDropdownLi(gig);
+      this.ul.appendChild(gigLi);
+    }
+  },
+  createDropdownLi: function (gig) {
+    var li = document.createElement('li');
+    li.innerText = gig.name;
+    return li;
   }
 }
 
