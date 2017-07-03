25863c5bd79c742d223bae9339b8066018528380
Louise Camlin <lacamlin@gmail.com>
2017-07-03 15:43:25 +0100

    refactor list prototype

--- client/src/views/list.js
+++ client/src/views/list.js
@@ -1,31 +1,40 @@
 var Gigs = require('../models/gigs.js');
 var MapWrapper = require('../mapWrapper.js');
 var DisplayInfo = require('./displayInfo.js');
+var Button = require('./button.js');
 var List = function () {
-
+  this.ul = document.createElement('ul');
 };
 
 List.prototype = {
   create: function () {
     var body = document.getElementsByTagName('body')[0]
     var listDiv = document.createElement('div')
-    var ul = document.createElement('ul');
     var gigs = new Gigs();
     gigs.all(function (gigs) {
       var gigs = JSON.parse(gigs);
       for (gig of gigs){
-        var gigListLi = document.createElement('li');
-        gigListLi.innerText = gig.name;
-        gigListLi.addEventListener('click', function(event){
-          var displayInfo = new DisplayInfo();
-          displayInfo.makeMapInfo(event);
-        })
-        ul.appendChild(gigListLi);
+        this.createListItem();
       }
-    });
-    listDiv.appendChild(ul);
+    }.bind(this));
+    listDiv.appendChild(this.ul);
     body.appendChild(listDiv);
-  }
+  },
+
+  createListItem: function() {
+    var gigListLi = document.createElement('li');
+    gigListLi.innerText = gig.name;
+    gigListLi.addEventListener('click', function(event){
+      var displayInfo = new DisplayInfo();
+      displayInfo.makeMapInfo(event);
+    })
+    this.ul.appendChild(gigListLi);
+   }
+  //
+  // createDeleteButton:
+
+
+
 
 }
 
