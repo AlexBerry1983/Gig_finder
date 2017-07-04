d096bc7bb025e242e466e710602157f90ed0b6c2
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-07-04 10:32:19 +0100

    refactor display info for create and append container

--- client/src/views/displayInfo.js
+++ client/src/views/displayInfo.js
@@ -6,25 +6,13 @@ var DisplayInfo = function(){
 
 DisplayInfo.prototype = {
   makeMapInfo: function(event){
-
     if (this.currentlyOpen == event.target) { return };
-
     if (this.currentlyOpen !== event.target && this.currentlyOpen !== undefined ){
-
       this.nukePopUps();
     }
-
-    var popUp = document.createElement('div');
-    this.makeMap(popUp, {lat: 55.953251, lng:-3.188267})
-    popUp.id = 'pop-up';
-
-    event.target.appendChild(popUp);
-
-
-    this.currentlyOpen = event.target;
-
-
+    this.createAndAppendContainer(event)
   },
+
   makeMap: function(el, center){
     var mainMap = new MapWrapper(el, center, 10);
     mainMap.addMarker(center);
@@ -36,7 +24,16 @@ DisplayInfo.prototype = {
       var li = existingPopUp.parentNode;
       li.removeChild(existingPopUp);
     }
+  },
+
+  createAndAppendContainer: function(){
+    var popUp = document.createElement('div');
+    this.makeMap(popUp, {lat: 55.953251, lng:-3.188267})
+    popUp.id = 'pop-up';
+    event.target.appendChild(popUp);
+    this.currentlyOpen = event.target;
   }
+
 }
 
 module.exports = DisplayInfo;
