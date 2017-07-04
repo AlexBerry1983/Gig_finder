22ed5a0a36e001764a442abe80f13b0b751b3473
User <user@Users-MacBook-Pro.local>
2017-07-04 16:36:17 +0100

    add modal box cancel and refactor

--- client/src/views/modalbox.js
+++ client/src/views/modalbox.js
@@ -7,7 +7,11 @@ var ModalBox = function(){
 
 ModalBox.prototype = {
   create: function(gig){
-    // console.log(gig);
+
+    if (this.currentBoxOption !== undefined) {
+      this.clear(this.currentBoxOption);
+    }
+
     var body = document.getElementsByTagName('body')[0];
     var jsonString = JSON.stringify(gig);
     var gigName = gig.name; 
@@ -16,6 +20,11 @@ ModalBox.prototype = {
     var url = '/api/listings';
 
     var confirmBox = document.createElement('div');
+    confirmBox.id = 'confirm-box';
+    this.currentBoxOption = confirmBox;
+
+    this.clear(confirmBox);
+
     var buttonSubmit = new Button();
     var confirmButton = buttonSubmit.create("post", url, function(){
       console.log("in post from submit button");
@@ -24,12 +33,28 @@ ModalBox.prototype = {
     var buttonCancel = document.createElement('button');
     buttonCancel.innerText = 'Cancel';
 
-    confirmBox.appendChild(pTag);
-    confirmBox.appendChild(confirmButton);
-    confirmBox.appendChild(buttonCancel);
+    // confirmBox.appendChild(pTag);
+    // confirmBox.appendChild(confirmButton);
+    // confirmBox.appendChild(buttonCancel);
+    this.append(confirmBox, pTag, confirmButton, buttonCancel);
     body.appendChild(confirmBox);
 
-  }
+  },
+
+  clear: function (node) {
+     while (node.hasChildNodes()) {
+       node.removeChild(node.children[0]);
+     }
+     node.remove();
+   },
+
+   append: function() {
+    for (var i =  1; i <= arguments.length - 1; i++) {
+      arguments[0].appendChild(arguments[i]);
+    }
+   }
+
+
 
 }
 
