ff4956d1c4b44b344682fe3c7fc516f1c214660e
Mungo Dewar <mpjdewar@gmail.com>
2017-07-05 14:10:33 +0100

    add checks if date, time or name is undefined that error msg shows

--- client/src/views/content.js
+++ client/src/views/content.js
@@ -12,17 +12,17 @@ Content.prototype = {
 
     var liTime = document.createElement('li');
     liTime.id = 'time';
-    liTime.textContent = gig.dates.start.localTime || undefined;
+    liTime.textContent = gig.dates.start.localTime || 'Not Found';
     ul.appendChild(liTime);
 
     var liDate = document.createElement('li');
     liDate.id = 'date';
-    liDate.textContent = gig.dates.start.localDate || undefined;
+    liDate.textContent = gig.dates.start.localDate || 'Not Found';
     ul.appendChild(liDate);
 
     var liPlace = document.createElement('li');
     liPlace.id = 'city';
-    liPlace.textContent = gig._embedded.venues[0].name || undefined;
+    liPlace.textContent = gig._embedded.venues[0].name || 'Not Found';
     ul.appendChild(liPlace);
 
     content.appendChild(ul);
